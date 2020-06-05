cd4373428c804b911fad1e9f8f4040dfee370283
Sikarwar, Rohit <rsikarwar@seic.com>
2020-06-05 16:51:34 -0400

    Reproduction for spyOn not detecting lifecycle methods.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,9 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+
 describe('AppComponent', () => {
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -10,22 +12,25 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  /**
+   * This test case FAILS, even though the ngOnInit method is getting called and console log is printed.
+   */
+  it('should call ngOnInit', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'spyon-failing-for-life-cycle-methods-angular9'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('spyon-failing-for-life-cycle-methods-angular9');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
+    const spyOnNgOnInit: jasmine.Spy = spyOn(component, 'ngOnInit').and.callThrough();
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('spyon-failing-for-life-cycle-methods-angular9 app is running!');
+    expect(spyOnNgOnInit).toHaveBeenCalled(); // FAILS
   });
+
+  /**
+   * PASSES.
+   */
+  // it('should call fun', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const component = fixture.componentInstance;
+  //   const spyOnFun: jasmine.Spy = spyOn(component, 'fun').and.callThrough();
+  //   fixture.detectChanges();
+  //   expect(spyOnFun).toHaveBeenCalled(); // PASSES
+  // });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'spyon-failing-for-life-cycle-methods-angular9';
+
+  ngOnInit(): void {
+    console.log('I am inside ngOnInit.');
+    this.fun();
+  }
+
+  fun(): void {
+    console.log('I am inside FUN.');
+  }
 }
